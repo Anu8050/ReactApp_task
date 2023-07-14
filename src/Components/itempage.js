bbced2a3270a199b800fbc3c378c755ff1d62b4a
Ausha M S <anusgowda050@gmail.com>
2023-07-14 11:51:24 +0530

    #1
    Change the top tab into left side tab in item page. -> fixed.

--- src/Components/itempage.js
+++ src/Components/itempage.js
@@ -1,29 +1,40 @@
 import React, { useState } from 'react';
-import {Box, Tabs, Tab} from '@mui/material';
+import { Box, Tabs, Tab, Dialog, DialogTitle, DialogContent, DialogActions, Button } from '@mui/material';
 
 function Itempage() {
-    const [value, setValue] = useState(0);
+  const [value, setValue] = useState(0);
+  const [dialogOpen, setDialogOpen] = useState(false);
 
-    //This method is for setting tab value.
-    const handleChange = (event, newValue) => 
-    {
-        setValue(newValue);
-    };
-    return (
-      <div className='itempage'>
-      <Tabs value={value} onChange={handleChange} aria-label="Item Tabs" >
-        <Tab label="Item One" />
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const openDialog = () => {
+    setDialogOpen(true);
+  };
+
+  const closeDialog = () => {
+    setDialogOpen(false);
+  };
+
+  return (
+    <div className='itempage'>
+      <Tabs orientation="vertical" value={value} onChange={handleChange} aria-label="Item Tabs">
+        <Tab label="Item One" onClick={openDialog} />
         <Tab label="Item Two" />
         <Tab label="Item Three" />
       </Tabs>
-      <Box style={{ display: 'flex', alignItems: 'flex-start' }}>
-        {value === 0 && <p>item one</p>}
-        {value === 1 && <p>item two</p>}
-        {value === 2 && <p>item three</p>}
-      </Box>
-      
-      
-      </div>
-    );
-  }
+      <Dialog open={dialogOpen} onClose={closeDialog}>
+        <DialogTitle>Item One Dialog</DialogTitle>
+        <DialogContent>
+          <p>This is the content of Item One dialog.</p>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={closeDialog} color="primary">Close</Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
+
 export default Itempage;
